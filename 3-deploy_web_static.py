014f00a0c07fadd1dd2b7e002dc2b252136fb0f0
haddyadnan <haddyadnan@gmail.com>
2022-10-13 16:22:33 +0100

    confirm docstring

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,6 +6,8 @@ Write a Fabric script (based on the file 2-do_deploy_web_static.py)
 that creates and distributes an archive to your web servers
 """
 
+
+
 from datetime import datetime
 from fabric.api import *
 import os
