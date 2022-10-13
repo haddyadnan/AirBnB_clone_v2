3f540c900311f587b9a5950db8621f476a83d8b8
haddyadnan <haddyadnan@gmail.com>
2022-10-13 13:07:30 +0100

    create local archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Write a Fabric script that generates a .tgz archive from the contents
+of the web_static folder of your AirBnB Clone repo
+"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """
+    Generates a .tgx archive from web_static
+    """
+    try:
+        local("mkdir -p versions")
+        fnme = f"versions/web_static_{datetime.now().strftime(('%Y%m%d%H%M%S'))}.tgz"
+        archive = local(f"tar -cvzf {fnme} web_static")
+        return fnme
+    except:
+        return None
